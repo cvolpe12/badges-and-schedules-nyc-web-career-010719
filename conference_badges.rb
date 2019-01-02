7b29c182ad962bc0a63a0f1455cf98517b2992f5
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 22:37:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(speakers)
+  speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  speakers.each do |names|
+    badge_maker(names)
+  end
+end
